cb2043f1bc1a00b1c12b256afcc2823910ad81ba
ianstranathan <34749333+ianstranathan@users.noreply.github.com>
2026-02-09 16:08:22 -0800

    cont

--- player/player_controller/remote_player_controller.gd
+++ player/player_controller/remote_player_controller.gd
@@ -11,6 +11,16 @@ func update_command(player_command_ref: PlayerCommand, _delta):
 	player_command_ref.using_controller = last_command.using_controller
 
 
+#@rpc("any_peer", "unreliable")
+#func receive_command(move: Vector2,
+					 #jump_pressed: bool,
+					 #jump_released: bool,
+					 #sequence_id: int) -> void:
+	#last_command.move_input = move
+	#last_command.jump_pressed = jump_pressed
+	#last_command.jump_released = jump_released
+	#last_command.sequence_id = sequence_id
+
 func inject_remote_intent(move: Vector2, 
 						  jump_pressed: bool,
 						  jump_released: bool) -> void:
